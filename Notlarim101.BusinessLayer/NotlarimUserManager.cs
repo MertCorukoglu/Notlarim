ef4ea4a7b923da11bd5ebb4cef85c4d96879d41f
rmzngns <rgunes57@gmail.com>
2021-12-28 13:47:15 +0300

    a

--- Notlarim101.BusinessLayer/NotlarimUserManager.cs
+++ Notlarim101.BusinessLayer/NotlarimUserManager.cs
@@ -35,7 +35,7 @@ namespace Notlarim101.BusinessLayer
             }
             else
             {
-                int dbResult = Insert(new NotlarimUser()
+                int dbResult = base.Insert(new NotlarimUser()
                 {
                     Name = data.Name,
                     Surname = data.Surname,
@@ -147,7 +147,7 @@ namespace Notlarim101.BusinessLayer
             {
                 res.Result.ProfileImageFilename = data.ProfileImageFilename;
             }
-            if (Update(res.Result) == 0)
+            if (base.Update(res.Result) == 0)
             {
                 res.AddError(ErrorMessageCode.ProfileCouldNotUpdate, "Profil güncellenemedi.");
             }
@@ -172,5 +172,71 @@ namespace Notlarim101.BusinessLayer
             }
             return res;
         }
+
+        //Hiding Method
+        public new BusinessLayerResult<NotlarimUser> Insert(NotlarimUser data)
+        {
+            NotlarimUser user = Find(s => s.Username == data.Username || s.Email == data.Email);
+            res.Result = data;
+
+            if (user != null)
+            {
+                if (user.Username == data.Username)
+                {
+                    res.AddError(ErrorMessageCode.UsernameAlreadyExist, "Kullanici adi kayitli");
+                }
+
+                if (user.Email == data.Email)
+                {
+                    res.AddError(ErrorMessageCode.EmailalreadyExist, "Email kayitli");
+                }
+                //throw new Exception("Kayitli kullanici yada e-posta adresi");
+            }
+            else
+            {
+                res.Result.ProfileImageFilename = "user1.jpeg";
+                res.Result.ActivateGuid = Guid.NewGuid();
+                if (base.Insert(res.Result)==0)
+                {
+                    res.AddError(ErrorMessageCode.UserCouldNotInserted, "Kullanıcı eklenemedi.");
+                }
+
+            }
+
+            return res;
+        }
+        public new BusinessLayerResult<NotlarimUser> Update(NotlarimUser data)
+        {
+            NotlarimUser user = Find(s => s.Id != data.Id && (s.Username == data.Username || s.Email == data.Email));
+
+            if (user != null && user.Id != data.Id)
+            {
+                if (user.Username == data.Username)
+                {
+                    res.AddError(ErrorMessageCode.UsernameAlreadyExist, "Bu kullanıcı adı daha önce kaydedilmiş.");
+                }
+                if (user.Email == data.Email)
+                {
+                    res.AddError(ErrorMessageCode.EmailalreadyExist, "Bu E-mail daha önce kaydedilmiş.");
+                }
+                return res;
+            }
+            res.Result = Find(s => s.Id == data.Id);
+            res.Result.Email = data.Email;
+            res.Result.Name = data.Name;
+            res.Result.Surname = data.Surname;
+            res.Result.Password = data.Password;
+            res.Result.Username = data.Username;
+            res.Result.IsActive = data.IsActive;
+            res.Result.IsAdmin = data.IsAdmin;
+
+            if (base.Update(res.Result) == 0)
+            {
+                res.AddError(ErrorMessageCode.UserCouldNotUpdated, "Kullanıcı güncellenemedi.");
+            }
+            return res;
+        }
+
     }
+
 }
--- Notlarim101.WebApp/Controllers/NotlarimUserController.cs
+++ Notlarim101.WebApp/Controllers/NotlarimUserController.cs
@@ -6,29 +6,28 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Notlarim101.BusinessLayer;
 using Notlarim101.Entity;
-using Notlarim101.WebApp.Data;
+
 
 namespace Notlarim101.WebApp.Controllers
 {
     public class NotlarimUserController : Controller
     {
-        private Notlarim101WebAppContext db = new Notlarim101WebAppContext();
-
-        // GET: NotlarimUser
+        private NotlarimUserManager num = new NotlarimUserManager();
         public ActionResult Index()
         {
-            return View(db.NotlarimUsers.ToList());
+            return View(num.List());
         }
 
-        // GET: NotlarimUser/Details/5
+
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            NotlarimUser notlarimUser = db.NotlarimUsers.Find(id);
+            NotlarimUser notlarimUser = num.Find(s => s.Id == id);
             if (notlarimUser == null)
             {
                 return HttpNotFound();
@@ -36,37 +35,41 @@ namespace Notlarim101.WebApp.Controllers
             return View(notlarimUser);
         }
 
-        // GET: NotlarimUser/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: NotlarimUser/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Surname,Username,Email,Password,ProfileImageFilename,IsActive,ActivateGuid,IsAdmin,CreatedOn,ModifiedOn,ModifiedUsername")] NotlarimUser notlarimUser)
+        public ActionResult Create(NotlarimUser notlarimUser)
         {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
             if (ModelState.IsValid)
             {
-                db.NotlarimUsers.Add(notlarimUser);
-                db.SaveChanges();
+                //username kontrolü ve email kontrolü yapılmalı
+                BusinessLayerResult<NotlarimUser> res = num.Insert(notlarimUser);
+                if (res.Errors.Count>0)
+                {
+                    res.Errors.ForEach(s => ModelState.AddModelError("",s.Message));
+                    return View(notlarimUser);
+                }
                 return RedirectToAction("Index");
             }
 
             return View(notlarimUser);
         }
 
-        // GET: NotlarimUser/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            NotlarimUser notlarimUser = db.NotlarimUsers.Find(id);
+            NotlarimUser notlarimUser = num.Find(s => s.Id == id);
             if (notlarimUser == null)
             {
                 return HttpNotFound();
@@ -74,30 +77,34 @@ namespace Notlarim101.WebApp.Controllers
             return View(notlarimUser);
         }
 
-        // POST: NotlarimUser/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Surname,Username,Email,Password,ProfileImageFilename,IsActive,ActivateGuid,IsAdmin,CreatedOn,ModifiedOn,ModifiedUsername")] NotlarimUser notlarimUser)
+        public ActionResult Edit(NotlarimUser notlarimUser)
         {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
             if (ModelState.IsValid)
             {
-                db.Entry(notlarimUser).State = EntityState.Modified;
-                db.SaveChanges();
+                BusinessLayerResult<NotlarimUser> res = num.Update(notlarimUser);
+                if (res.Errors.Count > 0)
+                {
+                    res.Errors.ForEach(s => ModelState.AddModelError("", s.Message));
+                    return View(notlarimUser);
+                }
                 return RedirectToAction("Index");
             }
             return View(notlarimUser);
         }
 
-        // GET: NotlarimUser/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            NotlarimUser notlarimUser = db.NotlarimUsers.Find(id);
+            NotlarimUser notlarimUser = num.Find(s => s.Id == id);
             if (notlarimUser == null)
             {
                 return HttpNotFound();
@@ -105,24 +112,13 @@ namespace Notlarim101.WebApp.Controllers
             return View(notlarimUser);
         }
 
-        // POST: NotlarimUser/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            NotlarimUser notlarimUser = db.NotlarimUsers.Find(id);
-            db.NotlarimUsers.Remove(notlarimUser);
-            db.SaveChanges();
+            NotlarimUser notlarimUser = num.Find(s => s.Id == id);
+            num.Delete(notlarimUser);
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
